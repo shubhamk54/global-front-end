2f46a380a990bf8ad932c2b57514162873712dcc
Shubham Kale <ShubhamKale54@gmail.com>
2019-09-26 10:27:25 +0530

    Snapshot, components and matcher tests added

---
+++ app/src/components/Badge/Badge.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Badge from './Badge.js';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+const fakeProps = { displayName: 'Active' };
+
+describe('Test Badge component', () => {
+
+    it('matches the snapshot', () => {
+        const tree = renderer.create(<Badge {...fakeProps} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('Badge renders the text inside it', () => {
+        const wrapper = mount(
+            <Badge {...fakeProps} />
+        );
+        const span = wrapper.find('.badge');
+        expect(span.text()).toBe(fakeProps.displayName);
+    });
+
+    test('Badge renders the text inside it', () => {
+        const wrapper = mount(
+            <Badge {...fakeProps} />
+        );
+        const span = wrapper.find('.badge');
+        expect(span.text()).not.toBe('Invalid');
+    });
+
+});
---
+++ app/src/components/DataTable/DataTable.test.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import DataTable from './DataTable.js';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+const fakeProps = {
+    columns: [
+        {
+            title: 'Id',
+            dataKey: 'id',
+        },
+        {
+            title: 'Name',
+            dataKey: 'name'
+        },
+        {
+            title: 'Start Date',
+            dataKey: 'startDate',
+            type: 'date',
+        },
+        {
+            title: 'End Date',
+            dataKey: 'endDate',
+            type: 'date',
+        },
+        {
+            title: 'Active',
+            dataKey: 'active',
+            type: 'pill',
+        },
+        {
+            title: 'Budget',
+            dataKey: 'Budget',
+            type: 'amount',
+        },
+    ],
+    data: [{ "id": 1, "name": "Divavu", "active": { title: 'Active', type: 'success' }, "startDate": "9/19/2017", "endDate": "3/9/2018", "Budget": 88377 },
+    { "id": 2, "name": "Jaxspan", "active": { title: 'Inactive', type: 'danger' }, "startDate": "11/21/2017", "endDate": "2/21/2018", "Budget": 608715 },
+    { "id": 3, "name": "Miboo", "active": { title: 'Active', type: 'success' }, "startDate": "11/1/2017", "endDate": "6/20/2017", "Budget": 239507 },
+    { "id": 4, "name": "Trilith", "active": { title: 'Inactive', type: 'danger' }, "startDate": "8/25/2017", "endDate": "11/30/2017", "Budget": 179838 },
+    { "id": 5, "name": "Layo", "active": { title: 'Active', type: 'success' }, "startDate": "11/28/2017", "endDate": "3/10/2018", "Budget": 837850 }],
+    currency: 'USD',
+};
+
+describe('Test DataTable component', () => {
+
+    it('matches the snapshot', () => {
+        const tree = renderer.create(<DataTable {...fakeProps} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('DataTable should not renders any table for empty data', () => {
+        const wrapper = mount(
+            <DataTable data={[]} columns={[]} />
+        );
+        const table = wrapper.find('.data-table');
+        expect(table.length).toBe(0);
+    });
+
+    test('single table should render for Given column and data', () => {
+        const wrapper = mount(
+            <DataTable {...fakeProps} />
+        );
+        const table = wrapper.find('.data-table');
+        expect(table.length).toBe(1);
+    });
+
+    test('Test for DataTable renders table column header for given list of column', () => {
+        const wrapper = mount(
+            <DataTable {...fakeProps} />
+        );
+        const tableColumns = wrapper.instance().renderTableHeader(fakeProps.columns);
+        expect(tableColumns).toHaveLength(fakeProps.columns.length);
+    });
+
+    test('Test for DataTable renders table data for given list of data', () => {
+        const wrapper = mount(
+            <DataTable {...fakeProps} />
+        );
+        const tableData = wrapper.instance().renderTableData(fakeProps.columns, fakeProps.data);
+        expect(tableData).toHaveLength(fakeProps.data.length);
+    });
+
+});
