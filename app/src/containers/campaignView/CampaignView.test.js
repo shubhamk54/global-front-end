db7278533f2aa0630779bdda2205095f627e3853
Shubham Kale <ShubhamKale54@gmail.com>
2019-09-27 03:18:08 +0530

    Push

--- app/src/containers/campaignView/CampaignView.test.js
+++ app/src/containers/campaignView/CampaignView.test.js
@@ -3,155 +3,153 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
-import CampaignView from './CampaignView.js';
 import renderer from 'react-test-renderer';
+import CampaignView from './CampaignView.js';
 
 const mockStore = configureMockStore();
 
-let store = mockStore({
-    data: {
-        campaignData: {
-            gridData: [],
-            dataDesc: "Please update criteria using available filters",
-            enableFilters: true,
-        }
-    }
+const store = mockStore({
+  data: {
+    campaignData: {
+      gridData: [],
+      dataDesc: 'Please update criteria using available filters',
+      enableFilters: true,
+    },
+  },
 });
 
 const fakeValiddata = [
-    {
-        id: 1, name: 'Divavu', active: { title: 'Active', type: 'success' }, startDate: '9/19/2017', endDate: '3/9/2018', Budget: 88377,
-    },
-    {
-        id: 2, name: 'Jaxspan', active: { title: 'Inactive', type: 'danger' }, startDate: '11/21/2017', endDate: '2/21/2018', Budget: 608715,
-    },
-    {
-        id: 3, name: 'Miboo', active: { title: 'Active', type: 'success' }, startDate: '11/1/2017', endDate: '6/20/2017', Budget: 239507,
-    },
-    {
-        id: 4, name: 'Trilith', active: { title: 'Inactive', type: 'danger' }, startDate: '8/25/2017', endDate: '11/30/2017', Budget: 179838,
-    },
-    {
-        id: 5, name: 'Layo', active: { title: 'Active', type: 'success' }, startDate: '11/28/2017', endDate: '3/10/2018', Budget: 837850,
-    }];
+  {
+    id: 1, name: 'Divavu', active: { title: 'Active', type: 'success' }, startDate: '9/19/2017', endDate: '3/9/2018', Budget: 88377,
+  },
+  {
+    id: 2, name: 'Jaxspan', active: { title: 'Inactive', type: 'danger' }, startDate: '11/21/2017', endDate: '2/21/2018', Budget: 608715,
+  },
+  {
+    id: 3, name: 'Miboo', active: { title: 'Active', type: 'success' }, startDate: '11/1/2017', endDate: '6/20/2017', Budget: 239507,
+  },
+  {
+    id: 4, name: 'Trilith', active: { title: 'Inactive', type: 'danger' }, startDate: '8/25/2017', endDate: '11/30/2017', Budget: 179838,
+  },
+  {
+    id: 5, name: 'Layo', active: { title: 'Active', type: 'success' }, startDate: '11/28/2017', endDate: '3/10/2018', Budget: 837850,
+  }];
 
 const fakeProps = {
-    columns: [
-        {
-            title: 'Id',
-            dataKey: 'id',
-        },
-        {
-            title: 'Name',
-            dataKey: 'name',
-        },
-        {
-            title: 'Start Date',
-            dataKey: 'startDate',
-            type: 'date',
-        },
-        {
-            title: 'End Date',
-            dataKey: 'endDate',
-            type: 'date',
-        },
-        {
-            title: 'Active',
-            dataKey: 'active',
-            type: 'pill',
-        },
-        {
-            title: 'Budget',
-            dataKey: 'Budget',
-            type: 'amount',
-        },
-    ],
-    data: [{
-        id: 1, name: 'Divavu', active: { title: 'Active', type: 'success' }, startDate: '9/19/2017', endDate: '3/9/2018', Budget: 88377,
+  columns: [
+    {
+      title: 'Id',
+      dataKey: 'id',
     },
     {
-        id: 2, name: 'Jaxspan', active: { title: 'Inactive', type: 'danger' }, startDate: '11/21/2017', endDate: '2/21/2018', Budget: 608715,
+      title: 'Name',
+      dataKey: 'name',
     },
     {
-        id: 3, name: 'Miboo', active: { title: 'Active', type: 'success' }, startDate: '11/1/2017', endDate: '6/20/2017', Budget: 239507,
+      title: 'Start Date',
+      dataKey: 'startDate',
+      type: 'date',
     },
     {
-        id: 4, name: 'Trilith', active: { title: 'Inactive', type: 'danger' }, startDate: '8/25/2017', endDate: '11/30/2017', Budget: 179838,
+      title: 'End Date',
+      dataKey: 'endDate',
+      type: 'date',
     },
     {
-        id: 5, name: 'Layo', active: { title: 'Active', type: 'success' }, startDate: '11/28/2017', endDate: '3/10/2018', Budget: 837850,
-    }],
-    fetchCampaignData: () => jest.fn(),
-    dataDesc: 'test test',
-    enableFilters: true,
+      title: 'Active',
+      dataKey: 'active',
+      type: 'pill',
+    },
+    {
+      title: 'Budget',
+      dataKey: 'Budget',
+      type: 'amount',
+    },
+  ],
+  data: [{
+    id: 1, name: 'Divavu', active: { title: 'Active', type: 'success' }, startDate: '9/19/2017', endDate: '3/9/2018', Budget: 88377,
+  },
+  {
+    id: 2, name: 'Jaxspan', active: { title: 'Inactive', type: 'danger' }, startDate: '11/21/2017', endDate: '2/21/2018', Budget: 608715,
+  },
+  {
+    id: 3, name: 'Miboo', active: { title: 'Active', type: 'success' }, startDate: '11/1/2017', endDate: '6/20/2017', Budget: 239507,
+  },
+  {
+    id: 4, name: 'Trilith', active: { title: 'Inactive', type: 'danger' }, startDate: '8/25/2017', endDate: '11/30/2017', Budget: 179838,
+  },
+  {
+    id: 5, name: 'Layo', active: { title: 'Active', type: 'success' }, startDate: '11/28/2017', endDate: '3/10/2018', Budget: 837850,
+  }],
+  fetchCampaignData: () => jest.fn(),
+  dataDesc: 'test test',
+  enableFilters: true,
 };
 
 describe('CampaignView snapshot testing', () => {
-    it('matches the snapshot', () => {
-        let wrapper;
-        wrapper = mount(
-            <Provider store={store}>
-                <CampaignView {...fakeProps} />
-            </Provider>,
-        );
-        wrapper.update();
-        const tree = renderer.create(wrapper).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it('matches the snapshot', () => {
+    let wrapper;
+    wrapper = mount(
+      <Provider store={store}>
+        <CampaignView {...fakeProps} />
+      </Provider>,
+    );
+    wrapper.update();
+    const tree = renderer.create(wrapper).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
 
 describe('CampaignView container testing with no grid data supplied yet', () => {
-    let wrapper;
+  let wrapper;
 
-    beforeEach(() => {
-        wrapper = mount(
-            <Provider store={store}>
-                <CampaignView {...fakeProps} />
-            </Provider>,
-        );
-    });
+  beforeEach(() => {
+    wrapper = mount(
+      <Provider store={store}>
+        <CampaignView {...fakeProps} />
+      </Provider>,
+    );
+  });
 
-    it('should contain start and end date calendar controls', () => {
-        const dayPickerInputs = wrapper.find('.DayPickerInput');
-        expect(dayPickerInputs.children()).toHaveLength(2);
-    });
+  it('should contain start and end date calendar controls', () => {
+    const dayPickerInputs = wrapper.find('.DayPickerInput');
+    expect(dayPickerInputs.children()).toHaveLength(2);
+  });
 
-    it('should contain input component for search', () => {
-        const searchInput = wrapper.find('.search-input');
-        expect(searchInput.children()).toHaveLength(2);
-    });
+  it('should contain input component for search', () => {
+    const searchInput = wrapper.find('.search-input');
+    expect(searchInput.children()).toHaveLength(2);
+  });
 
-    it('should contain no data available component', () => {
-        const noData = wrapper.find('.no-data');
-        expect(noData.children()).toHaveLength(2);
-    });
+  it('should contain no data available component', () => {
+    const noData = wrapper.find('.no-data');
+    expect(noData.children()).toHaveLength(2);
+  });
 });
 
 
 describe('CampaignView container testing when data is avilable', () => {
-    let wrapper;
-    let dataStore = mockStore({
-        data: {
-            campaignData: {
-                gridData: fakeValiddata,
-                dataDesc: "Please update criteria using available filters",
-                enableFilters: true,
-            }
-        }
-    });
+  let wrapper;
+  const dataStore = mockStore({
+    data: {
+      campaignData: {
+        gridData: fakeValiddata,
+        dataDesc: 'Please update criteria using available filters',
+        enableFilters: true,
+      },
+    },
+  });
 
-    beforeEach(() => {
+  beforeEach(() => {
+    wrapper = mount(
+      <Provider store={dataStore}>
+        <CampaignView {...fakeProps} />
+      </Provider>,
+    );
+  });
 
-        wrapper = mount(
-            <Provider store={dataStore}>
-                <CampaignView {...fakeProps} />
-            </Provider>,
-        );
-    });
-
-    it('should contain data table component', () => {
-        const dataTable = wrapper.find('.data-table');
-        expect(dataTable.children()).toHaveLength(1);
-    });
-
+  it('should contain data table component', () => {
+    const dataTable = wrapper.find('.data-table');
+    expect(dataTable.children()).toHaveLength(1);
+  });
 });
--- app/src/reducers/dataReducer.test.js
+++ app/src/reducers/dataReducer.test.js
@@ -1,86 +1,86 @@
-import dataReducer from './dataReducer'
+import dataReducer from './dataReducer';
 import * as types from '../actions/actionTypes';
 
 const fakeInititialState = {
-    gridData: [],
-    dataDesc: undefined,
+  gridData: [],
+  dataDesc: undefined,
 };
 
 const fakeValidGridData = [
-    { "id": 1, "name": "Photojam", "startDate": "7/25/2018", "endDate": "7/27/2019", "Budget": 858131 },
-    { "id": 2, "name": "Realbridge", "startDate": "03/05/2019", "endDate": "12/12/2019 ", "Budget": 505602 }
+  {
+    id: 1, name: 'Photojam', startDate: '7/25/2018', endDate: '7/27/2019', Budget: 858131,
+  },
+  {
+    id: 2, name: 'Realbridge', startDate: '03/05/2019', endDate: '12/12/2019 ', Budget: 505602,
+  },
 ];
 
-const fakeValidGridDataReduxResponse =
-{
-    campaignData: {
-        "dataDesc": "Please update criteria using available filters",
-        "enableFilters": true,
-        "gridData": fakeValidGridData,
-    }
-}
-const fakeInValidGridDataReduxResponse =
-{
-    "campaignData": {
-        "dataDesc": "Given data is not array.",
-        "gridData": [],
-    },
-}
+const fakeValidGridDataReduxResponse = {
+  campaignData: {
+    dataDesc: 'Please update criteria using available filters',
+    enableFilters: true,
+    gridData: fakeValidGridData,
+  },
+};
+const fakeInValidGridDataReduxResponse = {
+  campaignData: {
+    dataDesc: 'Given data is not array.',
+    gridData: [],
+  },
+};
 
 const actionOutputData = {
-    startDate: '09/09/2019',
-    endDate: '09/12/2019',
-    searchValue: 'a',
+  startDate: '09/09/2019',
+  endDate: '09/12/2019',
+  searchValue: 'a',
 };
 
 describe('tests for data reducer', () => {
-
-    it('should return the initial state', () => {
-        expect(dataReducer(undefined, {
-            type: types.FETCH_CAMPAIGN_DATA,
-        })).toEqual({
-            "campaignData": {},
-        });
+  it('should return the initial state', () => {
+    expect(dataReducer(undefined, {
+      type: types.FETCH_CAMPAIGN_DATA,
+    })).toEqual({
+      campaignData: {},
     });
+  });
 
-    it('should test fetch campaign data call from store', () => {
-
-        expect(
-            dataReducer(fakeInititialState, {
-                type: types.FETCH_CAMPAIGN_DATA,
-                campaignData: actionOutputData,
-            })
-        ).toEqual({
-            campaignData: actionOutputData
-        });
+  it('should test fetch campaign data call from store', () => {
+    expect(
+      dataReducer(fakeInititialState, {
+        type: types.FETCH_CAMPAIGN_DATA,
+        campaignData: actionOutputData,
+      }),
+    ).toEqual({
+      campaignData: actionOutputData,
     });
+  });
 
-    it('should test add campaign data call to the store', () => {
-        expect(
-            dataReducer(fakeInititialState, {
-                type: types.ADD_CAMPAIGN_DATA,
-                gridData: fakeValidGridData,
-                noDataMessage: 'Please update criteria using available filters',
-            })
-        ).toEqual(fakeValidGridDataReduxResponse);
-    });
+  it('should test add campaign data call to the store', () => {
+    expect(
+      dataReducer(fakeInititialState, {
+        type: types.ADD_CAMPAIGN_DATA,
+        gridData: fakeValidGridData,
+        noDataMessage: 'Please update criteria using available filters',
+      }),
+    ).toEqual(fakeValidGridDataReduxResponse);
+  });
 
 
-    it('should test add invalid campaign data to the store', () => {
-        expect(
-            dataReducer(fakeInititialState, {
-                type: types.INVALID_CAMPAIGN_DATA,
-                gridData: {},
-                validationMsg: 'Given data is not array.',
-            })
-        ).toEqual(fakeInValidGridDataReduxResponse);
-    });
+  it('should test add invalid campaign data to the store', () => {
+    expect(
+      dataReducer(fakeInititialState, {
+        type: types.INVALID_CAMPAIGN_DATA,
+        gridData: {},
+        validationMsg: 'Given data is not array.',
+      }),
+    ).toEqual(fakeInValidGridDataReduxResponse);
+  });
 
-    it('should match default case and return given current state', () => {
-        expect(
-            dataReducer(undefined, {
-                type: types.SOME_INVALID_ACTION,
-            })
-        ).toEqual({ campaignData: fakeInititialState });
-    });
+  it('should match default case and return given current state', () => {
+    expect(
+      dataReducer(undefined, {
+        type: types.SOME_INVALID_ACTION,
+      }),
+    ).toEqual({ campaignData: fakeInititialState });
+  });
 });
